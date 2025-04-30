a68fe90bc51d8b794d881b2ede2dca40dd6ed45f
Arman Radmanesh <radmanesh@gmail.com>
2025-04-30 16:29:00 -0500

    Add initial implementations of PSO and GA frameworks for Schwefel minimization problem

---
+++ problem_1.py
@@ -0,0 +1,250 @@
+#basic genetic algorithm Python code provided as base code for the DSA/ISE 5113 course
+#author: Charles Nicholson
+#date: 4/5/2019
+
+#NOTE: You will need to change various parts of this code.  However, please keep the majority of the code intact (e.g., you may revise existing logic/functions and add new logic/functions, but don't completely rewrite the entire base code!)
+#However, I would like all students to have the same problem instance, therefore please do not change anything relating to:
+#   random number generation
+#   number of items (should be 150)
+#   random problem instance
+#   weight limit of the knapsack
+
+#------------------------------------------------------------------------------
+
+#Student name: Arman Radmanesh, Ali Abdullah
+#Date: 04-29-2025
+
+
+#need some python libraries
+import copy
+import math
+from random import Random
+import numpy as np
+
+#to setup a random number generator, we will specify a "seed" value
+#need this for the random number generation -- do not change
+seed = 51132023
+myPRNG = Random(seed)
+
+#to get a random number between 0 and 1, use this:             myPRNG.random()
+#to get a random number between lwrBnd and upprBnd, use this:  myPRNG.uniform(lwrBnd,upprBnd)
+#to get a random integer between lwrBnd and upprBnd, use this: myPRNG.randint(lwrBnd,upprBnd)
+
+#number of elements in a solution (dimensions of Shwefel function)
+dimensions = 200
+
+#upper and lower bounds of the problem
+lowerBound = -500  #bounds for Schwefel Function search space
+upperBound = 500   #bounds for Schwefel Function search space
+
+
+#change anything you like below this line, but keep the gist of the program ------------------------------------
+
+#monitor the number of solutions evaluated
+solutionsChecked = 0
+
+
+populationSize = 500 #size of GA population
+Generations = 10000  #number of GA generations
+
+crossOverRate = 0.8  #currently not used in the implementation; needs to be used.
+mutationRate = 0.05  #currently not used in the implementation; needs to be used.
+eliteSolutions = int(populationSize * 0.02)  #currently not used in the implementation; need to use some type of elitism
+
+
+#create an continuous valued chromosome
+def createChromosome(d):
+    #this code as-is expects chromosomes to be stored as a list, e.g., x = []
+    #write code to generate chromosomes, most likely want this to be randomly generated
+    x = [myPRNG.uniform(lowerBound, upperBound) for _ in range(d)]
+    return x
+
+
+#create initial population by calling the "createChromosome" function many times and adding each to a list of chromosomes (a.k.a., the "population")
+def initializePopulation(): #n is size of population; d is dimensions of chromosome
+
+    population = []
+    populationFitness = []
+
+    for i in range(populationSize):
+        population.append(createChromosome(dimensions))
+        populationFitness.append(evaluate(population[i]))
+
+    tempZip = zip(population, populationFitness)
+    popVals = sorted(tempZip, key=lambda tempZip: tempZip[1])
+
+    #the return object is a reversed sorted list of tuples:
+    #the first element of the tuple is the chromosome; the second element is the fitness value
+    #for example:  popVals[0] is represents the best individual in the population
+    #popVals[0] for a 2D problem might be  ([-70.2, 426.1], 483.3)  -- chromosome is the list [-70.2, 426.1] and the fitness is 483.3
+
+    return popVals
+
+#implement a crossover
+def crossover(x1,x2):
+    # if the random number is greater than the crossover rate, then do not perform crossover
+    if(myPRNG.random() > crossOverRate):
+        return x1, x2
+
+    #x1 and x2 are the two parents
+    d = len(x1)  #get the number of dimensions in the chromosome
+    crossOverPt = myPRNG.randint(0, d-1)  #randomly select a crossover point
+    beta = myPRNG.random()  #create a new random number between 0 and 1, named "beta"
+    new1 = list(np.array(x1) - beta*(np.array(x1) - np.array(x2)))  #create a new list with the first alpha elements from x1
+    new2 = list(np.array(x2) + beta*(np.array(x1) - np.array(x2)))  #create a new list with the first alpha elements from x2
+
+    if(crossOverPt<d/2):
+        offspring1 = x1[0:crossOverPt] + new1[crossOverPt:d]  #create the first offspring
+        offspring2 = x2[0:crossOverPt] + new2[crossOverPt:d]  #create the second offspring
+    else:
+        offspring1 = new1[0:crossOverPt] + x1[crossOverPt:d]  #create the first offspring
+        offspring2 = new2[0:crossOverPt] + x2[crossOverPt:d]  #create the second offspring
+    #if no breeding occurs, then offspring1 and offspring2 can simply be copies of x1 and x2, respectively
+
+    return offspring1, offspring2  #two offspring are returned
+
+
+#Schwefel function to evaluate a real-valued solution x
+# note: the feasible space is an n-dimensional hypercube centered at the origin with side length = 2 * 500
+
+def evaluate(x):
+    val = 0
+    d = len(x)
+    for i in range(d):
+            val = val + x[i]*math.sin(math.sqrt(abs(x[i])))
+
+    val = 418.9829*d - val
+
+    return val          #returns the fitness value of the chromosome x
+
+
+
+
+
+#performs tournament selection; k chromosomes are selected (with repeats allowed) and the best advances to the mating pool
+#function returns the mating pool with size equal to the initial population
+def tournamentSelection(pop,k):
+
+    #randomly select k chromosomes; the best joins the mating pool
+    matingPool = []
+
+    while len(matingPool)<populationSize:
+
+        ids = [myPRNG.randint(0,populationSize-1) for i in range(k)]
+        competingIndividuals = [pop[i][1] for i in ids]
+        bestID=ids[competingIndividuals.index(min(competingIndividuals))]
+        matingPool.append(pop[bestID][0])
+
+    return matingPool
+
+
+
+#function to mutate solutions
+#this is a simple mutation function that adds a small random number to each element of the chromosome
+def mutate(x, epsilon=0.01):
+
+    #create some mutation logic  -- make sure to incorporate "mutationRate" somewhere and dont' do TOO much mutation
+    if myPRNG.random() < mutationRate:
+        for i in range(len(x)):
+            x[i] += x[i] * myPRNG.uniform(-epsilon, epsilon)  #add a small random number to the chromosome
+            if(x[i] < lowerBound):
+                x[i] = lowerBound
+            if(x[i] > upperBound):
+                x[i] = upperBound
+
+    return x
+
+
+
+
+#breeding -- uses the "mating pool" and calls "crossover" function
+def breeding(matingPool):
+    #the parents will be the first two individuals, then next two, then next two and so on
+
+    children = []
+    childrenFitness = []
+    for i in range(0,populationSize-1,2):
+        child1,child2=crossover(matingPool[i],matingPool[i+1])
+
+        child1=mutate(child1)
+        child2=mutate(child2)
+
+        children.append(child1)
+        children.append(child2)
+
+        childrenFitness.append(evaluate(child1))
+        childrenFitness.append(evaluate(child2))
+
+    tempZip = zip(children, childrenFitness)
+    popVals = sorted(tempZip, key=lambda tempZip: tempZip[1])
+
+    #the return object is a sorted list of tuples:
+    #the first element of the tuple is the chromosome; the second element is the fitness value
+    #for example:  popVals[0] is represents the best individual in the population
+    #popVals[0] for a 2D problem might be  ([-70.2, 426.1], 483.3)  -- chromosome is the list [-70.2, 426.1] and the fitness is 483.3
+
+    return popVals
+
+#insertion step
+def insert(pop,kids):
+
+    #this is not a good solution here... essentially this is replacing the previous generation with the offspring and not implementing any type of elitism
+    #at the VERY LEAST evaluate the best solution from "pop" to make sure you are not losing a very good chromosome from last generation
+    #maybe want to keep the top 5? 10? solutions from pop -- it's up to you.
+
+    # Check the k best solutions from the previous generation and keep them in the new generation if they are better than the new ones
+    for i in range(eliteSolutions):
+        if pop[i][1] < kids[i][1]:
+            kids[i] = pop[i]
+
+    return kids
+
+
+
+#perform a simple summary on the population: returns the best chromosome fitness, the average population fitness, and the variance of the population fitness
+def summaryFitness(pop):
+    a=np.array(list(zip(*pop))[1])
+    return np.min(a), np.mean(a), np.var(a)
+
+
+#the best solution should always be the first element...
+def bestSolutionInPopulation(pop):
+    print ("Best solution: ", pop[0][0])
+    print ("Value: ", pop[0][1])
+
+
+
+def main():
+    #GA main code
+    Population = initializePopulation()
+
+
+    #optional: you can output results to a file -- i've commented out all of the file out put for now
+    #f = open('out.txt', 'w')  #---uncomment this line to create a file for saving output
+
+
+    for j in range(Generations):
+
+        mates=tournamentSelection(Population,3)  #<--need to replace this with roulette wheel selection, e.g.:  mates=rouletteWheel(Population)
+        # mates = rouletteWheel(Population)
+        Offspring = breeding(mates)
+        Population = insert(Population, Offspring)
+
+        #end of GA main code
+
+        minVal, meanVal, varVal=summaryFitness(Population)          #check out the population at each generation
+        print("Iteration: ", j, " min:", minVal, " mean:", meanVal, " var:", varVal)
+
+        #f.write(str(minVal) + " " + str(meanVal) + " " + str(varVal) + "\n")  #---uncomment this line to write to  file
+
+    #f.close()   #---uncomment this line to close the file for saving output
+
+    print (summaryFitness(Population))
+    bestSolutionInPopulation(Population)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ problem_2.py
@@ -0,0 +1,112 @@
+#the intial framework for a particle swarm optimization for Schwefel minimization problem
+#author: Charles Nicholson
+#for ISE/DSA 5113
+
+
+#need some python libraries
+import copy
+import math
+from random import Random
+
+
+#to setup a random number generator, we will specify a "seed" value
+seed = 12345
+myPRNG = Random(seed)
+
+#to get a random number between 0 and 1, write call this:             myPRNG.random()
+#to get a random number between lwrBnd and upprBnd, write call this:  myPRNG.uniform(lwrBnd,upprBnd)
+#to get a random integer between lwrBnd and upprBnd, write call this: myPRNG.randint(lwrBnd,upprBnd)
+
+lowerBound = -500  #bounds for Schwefel Function search space
+upperBound = 500   #bounds for Schwefel Function search space
+
+#you may change anything below this line that you wish too -----------------------------------------------------
+
+#note: for the more experienced Python programmers, you might want to consider taking a more object-oriented approach to the PSO implementation, i.e.: a particle class with methods to initialize itself, and update its own velocity and position; a swarm class with a method to iterates through all particles to call update functions, etc.
+
+#number of dimensions of problem
+dimensions = 2
+
+#number of particles in swarm
+swarmSize = 10
+
+
+#Schwefel function to evaluate a real-valued solution x
+# note: the feasible space is an n-dimensional hypercube centered at the origin with side length = 2 * 500
+
+def evaluate(x):
+      val = 0
+      d = len(x)
+      for i in range(d):
+            val = val + x[i]*math.sin(math.sqrt(abs(x[i])))
+
+      val = 418.9829*d - val
+
+      return val
+
+# gunction that returns the global best position of the swarm
+def getGlobalBest(pBest, pBestVal):
+      gBest = pBest[0][:]  #initialize gBest to the first particle's best position
+      gBestVal = pBestVal[0]  #initialize gBestVal to the first particle's best value
+
+      for i in range(1, len(pBest)):
+            if pBestVal[i] < gBestVal:
+                  gBest = pBest[i][:]
+                  gBestVal = pBestVal[i]
+
+      return gBest, gBestVal
+
+# velocity update function
+def updateVelocity(vel, pos, pBest, gBest, w, c1, c2):
+      for i in range(len(vel)):
+            for j in range(len(vel[i])):
+                  r1 = myPRNG.random()
+                  r2 = myPRNG.random()
+                  vel[i][j] = (w * vel[i][j]) + (c1 * r1 * (pBest[i][j] - pos[i][j])) + (c2 * r2 * (gBest[j] - pos[i][j]))
+      return vel
+
+#the swarm will be represented as a list of positions, velocities, values, pbest, and pbest values
+
+pos = [[] for _ in range(swarmSize)]      #position of particles -- will be a list of lists; e.g., for a 2D problem with 3 particles: [[17,4],[-100,2],[87,-1.2]]
+vel = [[] for _ in range(swarmSize)]      #velocity of particles -- will be a list of lists similar to the "pos" object
+
+#note: pos[0] and vel[0] provides the position and velocity of particle 0; pos[1] and vel[1] provides the position and velocity of particle 1; and so on.
+
+
+curValue = [] #evaluation value of current position  -- will be a list of real values; curValue[0] provides the evaluation of particle 0 in it's current position
+pbest = []    #particles' best historical position -- will be a list of lists: pbest[0] provides the position of particle 0's best historical position
+pbestVal = [] #value of pbest position  -- will be a list of real values: pbestBal[0] provides the value of particle 0's pbest location
+
+
+#initialize the swarm randomly
+for i in range(swarmSize):
+      for j in range(dimensions):
+            pos[i].append(myPRNG.uniform(lowerBound,upperBound))    #assign random value between lower and upper bounds
+            vel[i].append(myPRNG.uniform(-1,1))                     #assign random value between -1 and 1   --- maybe these are good bounds?  maybe not...
+
+      curValue.append(evaluate(pos[i]))   #evaluate the current position
+
+pBest = pos[:]          # initialize pbest to the starting position
+pBestVal = curValue[:]  # initialize pbest to the starting position
+
+# main loop to find the global best position
+done = False
+
+while not done:
+        # evaluate the swarm
+        for i in range(swarmSize):
+              curValue[i] = evaluate(pos[i])  #evaluate the current position
+
+              #update pbest if the current value is better than the historical best
+              if curValue[i] < pBestVal[i]:
+                    pBest[i] = pos[i][:]  #copy the current position to the pbest position
+                    pBestVal[i] = curValue[i]  #copy the current value to the pbest value
+
+#Currently missing several elements
+#e.g., velocity update function; velocity max limitations; position updates; dealing with infeasible space; identifying the global best; main loop, stopping criterion, etc.
+
+
+
+
+
+
